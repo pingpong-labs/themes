40ea65814cfcae0c4d0793657545e86546e9b240
Mahmut Bayri <mahmutbayri@gmail.com>
2015-11-17 23:51:28 +0200

    The asset url base will be get browser path with the url function in Laravel. So, you don't have to specify base url in app.php. It is useful for development and testing cases.
    Remove slash at the begining.
    
    Fix getting theme path.
    If theme path isn't set with setPath method, getThemePath method return only theme name.

--- Repository.php
+++ Repository.php
@@ -118,7 +118,7 @@ class Repository implements Arrayable
      */
     public function getThemePath($theme)
     {
-        return $this->path."/{$theme}";
+        return $this->getPath() . "/{$theme}";
     }
 
     /**
@@ -359,32 +359,32 @@ class Repository implements Arrayable
     {
         return $this->views->make($this->getThemeNamespace($view), $data, $mergeData);
     }
-    
+
     /**
      * Get asset for theme
-     * 
+     *
      * @param $asset
      * @param $secure
-     * 
+     *
      * @return asset
      */
-    public function asset($asset, $secure = null) 
+    public function asset($asset, $secure = null)
     {
-        return app('url')->asset(config('app.url') . '/themes/' . $this->getCurrent() . '/' . $asset, $secure);
+        return url('themes/' . $this->getCurrent() . '/' . $asset, null, $secure);
     }
-    
+
     /**
      * Get secure asset
-     * 
+     *
      * @param $asset
-     * 
+     *
      * @return asset
      */
     public function secure_asset($asset)
     {
-        return app('url')->asset(config('app.url') . '/themes/' . $this->getCurrent() . '/' . $asset);
+        return $this->asset($asset, true);
     }
-    
+
     /**
      * Register view location of theme.
      *
